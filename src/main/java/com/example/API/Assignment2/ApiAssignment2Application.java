0426d36f520da7097f71b8e370e6098a288fb849
Bunroung Heak <b_heak@uncg.edu>
2024-02-04 20:30:49 -0500

    Initial commit

---
+++ src/main/java/com/example/API/Assignment2/ApiAssignment2Application.java
@@ -0,0 +1,13 @@
+package com.example.API.Assignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAssignment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAssignment2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/API/Assignment2/ApiAssignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API.Assignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAssignment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
